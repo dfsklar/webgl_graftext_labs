fd74999928d4979cd516c29e73f576205f72d1ee
dfsklar <dfsklar@gmail.com>
2017-08-15 23:52:07 -0400

    just indentation

--- foundapp02/trackball_glmatrix.js
+++ foundapp02/trackball_glmatrix.js
@@ -29,6 +29,10 @@ window.vec3extension = {
 
 window.TrackballControls = function ( camera, domElement ) {
 
+  /* 
+     camera must have:  position, up 
+   */
+
 	var _this = this;
 	var STATE = { NONE: - 1, ROTATE: 0, ZOOM: 1, PAN: 2, TOUCH_ROTATE: 3, TOUCH_ZOOM_PAN: 4 };
 
